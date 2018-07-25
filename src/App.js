398499e61087b35ed849d1bd31053726f6f5ed36
Patrick Bower <bower.patrick@gmail.com>
2018-07-25 23:21:26 +0100

    Chore (project setup) Routing and Linting
    Add react-router with example and
    eslint auto fix with .rc file.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/topics">Topics</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          {/* <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/topics" component={Topics} /> */}
+          <Route exact path="/" render={() => <h1>Home</h1>} />
+          <Route path="/about" render={() => <h1>About</h1>} />
+          <Route path="/topics" render={() => <h1>Topics</h1>} />
+        </div>
+      </Router>
     );
   }
 }
