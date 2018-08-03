a8c2eb67866bbfa1b94f9e5a61ee44f240629b7e
Patrick Bower <bower.patrick@gmail.com>
2018-08-03 09:59:02 +0100

    feat(Grid system): Add responsive css grid layout in Article and Contents components

---
+++ src/components/ArticleInfo/ArticleInfo.js
@@ -0,0 +1,20 @@
+import React from "react";
+import style from "./ArticleInfo.module.css";
+
+/**
+ * @function ArticleInfo
+ * Publishing information about the current article
+ *
+ */
+
+const ArticleInfo = props => {
+  console.log(props);
+  return (
+    <p className={style.infoText}>
+      Published {props.data.dateDay}
+      {props.data.dateOrdinal} {props.data.dateMonth}
+    </p>
+  );
+};
+
+export default ArticleInfo;
